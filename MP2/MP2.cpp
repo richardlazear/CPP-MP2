e2b45da5d62d80f0daa4fab59324a1a1229f82ff
richardlazear <richard.g.lazear@gmail.com>
2015-01-22 11:11:53 -0600

    in-class 1/22...insertNode half-working

--- MP2/MP2.cpp
+++ MP2/MP2.cpp
@@ -1,4 +1,4 @@
-/******************************************************************************************************************
+/***********************************************************************************************************************
 Create a node with one string in it and a pointer to point to the next node
 Create a function to add a node to the end of the list
 Create a function to traverse the list
@@ -8,10 +8,11 @@ In the main, read in an unknown number of strings from the user
 When they are done entering strings, traverse the list
 Create a linear search function that traverses the list looking for the search string
 Create an insert node function that uses the search method to insert a node in to the list
+	If the string is found, insert a node right after the found node.  If not, insert a new node at the end of the list
 	Should handle inserting into the beginning, middle, and end of the list
 Create a delete function that uses the search function to remove a node from the list
 	Should handle deleting from the beginning, middle, and end of the list
-******************************************************************************************************************/
+***********************************************************************************************************************/
 
 // http://www.cprogramming.com/tutorial/lesson15.html
 // http://www.cprogramming.com/snippets/source-code/singly-linked-list-insert-remove-add-count
@@ -33,7 +34,7 @@ void addNode(node * &, string); // adds a node to the end of the list
 void printNode(node *); // prints a single struct node
 void traverse(node *); // traverses the list and prints out contents in the nodes
 node * searchList(node * &, string); // searches the list for a user-entered string, returns the pointer to the "found" struct node
-void insertNode(); // inserts a node in to the list
+void insertNode(node * &, string); // inserts a node in to the list
 void deleteNode(node * &, string); // deletes a node from anywhere in the list
 
 int main()
@@ -66,7 +67,11 @@ int main()
 	traverse(root);
 
 	// TEST testing the search method
-	node *myTest = searchList(root, "joe");
+	node *mySearchTest = searchList(root, "joe");
+
+	// TEST testing the insert method
+	insertNode(root, "joe");
+	traverse(root);
 
 	system("PAUSE");
 	return 0;
@@ -122,12 +127,44 @@ node * searchList(node * &searchStart, string toFind)
 				conductor = conductor->nextRec;
 			}
 		}
+		cout << "That string was not found.";
+		return NULL;
 	}
 }
 
-void insertNode()
+void insertNode(node * &listStart, string toFind)
 {
-
+	cout << "Please choose where you want to insert a new node.  \"0\" for the start of the list, \"1\" for anywhere else in the list: ";
+	int choice;
+	cin >> choice;
+	if (choice == 0)
+	{
+		node *newStart;
+		newStart = NULL;
+		// Add the new string to the new node
+		cout << "Please enter a string for the new node: ";
+		string input;
+		cin >> input;
+		newStart->myString = input;
+		newStart->nextRec = listStart;
+	}
+	else
+	{
+		node *insertLocation = searchList(listStart, toFind);
+		if (insertLocation != NULL)
+		{
+			node *temp = insertLocation->nextRec;
+			insertLocation->nextRec = new node; // Have the insertLocation node point to the new node being inserted
+			insertLocation = insertLocation->nextRec;
+			// Add the new string to the new node
+			cout << "Please enter a string for the new node: ";
+			string input;
+			cin >> input;
+			insertLocation->myString = input;
+			// Point the newly inserted node to the next node
+			insertLocation->nextRec = temp;
+		}
+	}
 }
 
 void deleteNode(node * &listStart, string toFind)
